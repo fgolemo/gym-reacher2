7cd5405ffaf120008287029733c0f59bd8bea132
Florian Golemo <fgolemo@gmail.com>
2017-05-29 22:11:54 +0200

    finished testing case

--- gym_vrep/envs/vrepper-test.py
+++ gym_vrep/envs/vrepper-test.py
@@ -1,59 +1,71 @@
+import os
+import time
+
 from vrepper.vrepper import vrepper
-
-import os,time
-import numpy as np
-
 
 venv = vrepper(headless=False)
 venv.start()
-# time.sleep(3)
 venv.load_scene(os.getcwd() + '/../scenes/poppy_ergo_jr_vanilla_ball.ttt')
-venv.start_blocking_simulation()
-# self.slider = venv.get_object_by_name('slider')
-# self.cart = venv.get_object_by_name('cart')
-# self.mass = venv.get_object_by_name('mass')
 
-print('(CartPoleVREP) initialized')
-time.sleep(2)
 
-# cartpos = self.cart.get_position()
-# masspos = self.mass.get_position()
-# cartvel,cart_angvel = self.cart.get_velocity()
-# massvel,mass_angvel = self.cart.get_velocity()
-#
-# self.observation = np.array([
-#     cartpos[0],cartvel[0],
-#     masspos[0],masspos[2],
-#     massvel[0],massvel[2]
-#     ]).astype('float32')
-#
-# actions = np.clip(actions, -1, 1)
-# v = actions[0]
-#
-# # step
-# self.slider.set_velocity(v)
-# self.venv.step_blocking_simulation()
-#
-# # observe again
-# self._self_observe()
-#
-# # cost
-# height_of_mass = self.observation[3] # masspos[2]
-# cost = - height_of_mass + (v**2) * 0.001
-#
-#
-# def _reset(self):
-#     self.venv.stop_blocking_simulation()
-#     self.venv.start_blocking_simulation()
-#     self._self_observe()
-#     return self.observation
-#
-# def _destroy(self):
-#     self.venv.stop_blocking_simulation()
-#     self.venv.end()
-#
-#
-# print('simulation ended. leaving in 3 seconds...')
-# time.sleep(5)
+motors = []
+for i in range(6):
+    motor = venv.get_object_by_name('m{}'.format(i + 1), is_joint=True)
+    motors.append(motor)
+
+restMotors = [0, -90, 35, 0, 55, -90]
+
+
+def reset():
+    venv.stop_simulation()
+    venv.start_nonblocking_simulation()
+
+    print "SETTING MOTORS"
+
+    for i, m in enumerate(motors):
+        m.set_position_target(restMotors[i])
+
+    print "SPAWNING BALL"
+
+    params = venv.create_params(
+        [],
+        [.035, .035, .035,  # size
+         0, 0.05, .3,  # position
+         .01],  # weight)
+        [],
+        ''
+    )
+    test = venv.call_script_function('spawnBall', params)
+    print test
+    time.sleep(.5)
+    print "MAKING SIM SYNC"
+
+    venv.make_simulation_synchronous(True)
+
+    print('(Ergo ball scene) initialized')
+
+
+throwStart = [0, 40, 40, 0, -90, -55]
+throwEnd = [0, -50, -40, 0, -50, -30]
+
+def gotoPos(pos):
+    for i, m in enumerate(motors):
+        m.set_position_target(pos[i])
+
+
+
+for i in range(2):
+    reset()
+    ball = venv.get_object_by_name("ball")
+    gotoPos(throwStart) # start pos
+    for _ in range(30):
+        venv.step_blocking_simulation()
+        print ball.get_position()
+    gotoPos(throwEnd) # end pos
+    for _ in range(30):
+        venv.step_blocking_simulation()
+        print ball.get_position()
+    time.sleep(1)
+
 venv.stop_blocking_simulation()
 venv.end()
