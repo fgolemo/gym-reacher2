a6706041f7690fd9d44d0313dd8edf58a89fe1ba
Florian Golemo <fgolemo@gmail.com>
2017-06-07 23:13:11 +0200

    corrected version number

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name='gym_vrep',
       version='0.0.1',
       install_requires=['gym>=0.2.3',
-                        'vrepper>=0.0.1']
+                        'vrepper>=0.0.3']
 )
