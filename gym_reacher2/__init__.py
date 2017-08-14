69d4cee3cc440e3e1e5cf387191d73fc1dee67a1
Florian Golemo <fgolemo@gmail.com>
2017-08-14 18:37:20 -0400

    changed max environment steps from 50 to 500

--- gym_reacher2/__init__.py
+++ gym_reacher2/__init__.py
@@ -3,7 +3,7 @@ from gym.envs.registration import register
 register(
     id='Reacher2-v1',
     entry_point='gym_reacher2.envs:Reacher2Env',
-    max_episode_steps=50,
+    max_episode_steps=500,
     reward_threshold=-3.75,
 )
 
