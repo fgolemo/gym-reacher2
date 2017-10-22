f2a8ee5cc7b5b00673388383a645a7e6a29344e2
Florian Golemo <fgolemo@gmail.com>
2017-10-22 15:52:29 -0400

    added (shitty) bigger reacher-plus

--- gym_reacher2/__init__.py
+++ gym_reacher2/__init__.py
@@ -18,3 +18,8 @@ register(
     entry_point='gym_reacher2.envs:Reacher2PlusEnv',
     kwargs={'base_env_id': 'Reacher2-v1'}
 )
+register(
+    id='Reacher2PlusBig-v1',
+    entry_point='gym_reacher2.envs:Reacher2PlusBigEnv',
+    kwargs={'base_env_id': 'Reacher2-v1'}
+)
--- gym_reacher2/envs/__init__.py
+++ gym_reacher2/envs/__init__.py
@@ -2,3 +2,5 @@ from gym_reacher2.envs.mujoco_env import MujocoReacher2Env
 from gym_reacher2.envs.reacher2 import Reacher2Env
 from gym_reacher2.envs.reacher2pixel import Reacher2PixelEnv
 from gym_reacher2.envs.reacher2plus import Reacher2PlusEnv
+from gym_reacher2.envs.reacher2plusbig import Reacher2PlusBigEnv
+
