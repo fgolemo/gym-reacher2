248b9715a96045a8a7dd7b06066ba3e711c64b76
Adrien Ali Taïga <adrien.alitaiga@gmail.com>
2017-12-12 11:44:05 -0500

    Update __init__.py

--- gym_reacher2/__init__.py
+++ gym_reacher2/__init__.py
@@ -3,7 +3,8 @@ from gym.envs.registration import register
 register(
     id='Reacher2-v0',
     entry_point='gym_reacher2.envs:Reacher2Env',
-    max_episode_steps=500,
+    # Set to 50 for learning, might have to set higher for collecting data
+    max_episode_steps=50,
     reward_threshold=-3.75,
 )
 
